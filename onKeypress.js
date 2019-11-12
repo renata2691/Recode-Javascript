52ef97f7c8c0ce63f3e3b699cd1fbca8d8161abc
renata2691 <48625125+renata2691@users.noreply.github.com>
2019-11-12 17:40:46 -0300

    Aula 01 - Javascript
    Manipulação de DOM, Eventos e funções

---
+++ onKeypress.js
@@ -0,0 +1,3 @@
+function myFunction(){
+    alert("You pressed a key inside the input field");
+}
---
+++ onclick.js
@@ -0,0 +1,5 @@
+
+    function myFunction() {
+        document.getElementById("demo").innerHTML = "YOU CLICKED ME!";       
+    }
+
